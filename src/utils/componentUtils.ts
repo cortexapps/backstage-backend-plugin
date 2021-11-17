4c718a8e93507d205cac39a12f5f5a7a51a558ce
Nikhil Unni <nikhil.unni@getcortexapp.com>
2021-11-17 13:07:54 -0800

    Add extension API to entity sync (#2)

---
+++ src/utils/componentUtils.ts
@@ -0,0 +1,57 @@
+/*
+ * Copyright 2021 Cortex Applications, Inc.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+import { Entity } from '@backstage/catalog-model';
+import { CustomMapping } from '@cortexapps/backstage-plugin-extensions';
+import { merge } from 'lodash';
+
+export type EntityRefContext = {
+  defaultKind?: string;
+  defaultNamespace?: string;
+};
+
+export const defaultComponentRefContext: EntityRefContext = {
+  defaultKind: 'Component',
+  defaultNamespace: 'default',
+};
+
+export const defaultGroupRefContext: EntityRefContext = {
+  defaultKind: 'Group',
+  defaultNamespace: 'default',
+};
+
+export const defaultSystemRefContext: EntityRefContext = {
+  defaultKind: 'System',
+  defaultNamespace: 'default',
+};
+
+export const applyCustomMappings = (
+  entity: Entity,
+  customMappings: CustomMapping[],
+) => {
+  return customMappings.reduce(
+    (modifiedEntity: Entity, customMapping: CustomMapping) => {
+      const partialEntity = {
+        spec: customMapping(entity),
+      };
+
+      merge(modifiedEntity, partialEntity);
+
+      return modifiedEntity;
+    },
+    entity,
+  );
+};
