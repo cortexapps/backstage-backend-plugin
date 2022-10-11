378d6a74330d450e8b3ece9aa63677dbbe54ef94
Nikhil Unni <nikhil.unni@cortex.io>
2022-10-11 11:59:21 -0700

    fix lint

--- rollup.config.js
+++ rollup.config.js
@@ -1,10 +1,25 @@
-/* eslint-disable import/no-extraneous-dependencies */
+/*
+ * Copyright 2022 Cortex Applications, Inc.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
 import resolve from '@rollup/plugin-node-resolve';
 import esbuild from 'rollup-plugin-esbuild';
 import peerDepsExternal from 'rollup-plugin-peer-deps-external';
 import commonjs from '@rollup/plugin-commonjs';
 import dts from 'rollup-plugin-dts';
 
+// eslint-disable-next-line import/no-anonymous-default-export
 export default [{
   input: 'src/index.ts',
   output: {
